82a06a3ecc815e9692f2174d6ce741cd74c73da5
labidi-firas <firaslabidi17@gmail.com>
2024-02-15 18:11:05 +0100

    third commit

--- src/Form/JobsCategoryType.php
+++ src/Form/JobsCategoryType.php
@@ -6,16 +6,22 @@ use App\Entity\JobsCategory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class JobsCategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('category_name')
-        ;
+            ->add('category_name', null, [
+                'constraints' => [
+                    new Length([
+                        'max' => 30,
+                        'maxMessage' => 'The category name should not be longer than {{ limit }} characters.',
+                    ]),
+                ],
+            ]);
     }
-    
 
     public function configureOptions(OptionsResolver $resolver)
     {
